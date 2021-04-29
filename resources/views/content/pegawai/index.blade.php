108d028f5e0e8790f519eb608fbde79aa4d01093
dyah03 <trianadyahpangestuti@gmail.com>
2021-04-29 21:04:43 +0700

    perbaikan kategori

--- resources/views/content/pegawai/index.blade.php
+++ resources/views/content/pegawai/index.blade.php
@@ -56,7 +56,7 @@
                                     @foreach ($transaksi_detail as $transaksi_detail)
                                         <tr>
                                             <td>{{ $loop->iteration }}</td>
-                                            <td>{{ $transaksi_detail->produk->nama }}</td>
+                                            <td>{{ $transaksi_detail->produk->kategori->nama.'-'.$transaksi_detail->produk->nama }}</td>
                                             <td>{{ $transaksi_detail->jumlah_beli }} Pcs</td>
                                             <td>@currency($transaksi_detail->produk->harga)</td>
                                             <td>@currency($transaksi_detail->jumlah_harga)</td>
