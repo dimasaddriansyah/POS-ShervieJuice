c736777c388c1237ccddb7278251ad0058ca8090
zulfakhoerul <dimasaddriansyah14@gmail.com>
2021-06-05 14:05:29 +0700

    Fix Laporan Excel Transaksi dan Keuangan AutoSize

--- app/Exports/TransaksiExport.php
+++ app/Exports/TransaksiExport.php
@@ -5,8 +5,9 @@ namespace App\Exports;
 use App\Models\Transaksi;
 use Illuminate\Contracts\View\View;
 use Maatwebsite\Excel\Concerns\FromView;
+use Maatwebsite\Excel\Concerns\ShouldAutoSize;
 
-class TransaksiExport implements FromView
+class TransaksiExport implements FromView,ShouldAutoSize
 {
     public function view(): View
     {
